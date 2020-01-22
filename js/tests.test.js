229fb1f270d56bb1ebc2cd4aa1489a3250e10ccc
MephistoDevelop <inesherald@gmail.com>
2020-01-21 23:07:12 -0600

    Add reverseTring Function and Test

--- js/tests.test.js
+++ js/tests.test.js
@@ -1,5 +1,10 @@
 const capitalizeWord = require('./capitalize.js');
+const reverseWord = require('./reverseString.js');
 
 test('Capitalize an input String', () => {
   expect(capitalizeWord('mephistoDevelop')).toBe('MephistoDevelop');
+});
+
+test('Reverse a sting', () => {
+  expect(reverseWord('Hello World')).toBe('dlroW olleH');
 });
