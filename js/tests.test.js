5f443a1cb34f5b9d0860d0304f71466862191722
MephistoDevelop <inesherald@gmail.com>
2020-01-23 07:47:10 -0600

    Fix linter file

--- js/tests.test.js
+++ js/tests.test.js
@@ -2,7 +2,7 @@ const capitalizeWord = require('./capitalize.js');
 const reverseWord = require('./reverseString.js');
 const calculate = require('./calculator.js');
 const caesarCipher = require('./caesarCipher.js');
-const arrayAnalysis = require('./arrayAnalisis.js')
+const arrayAnalysis = require('./arrayAnalisis.js');
 
 test('Capitalize an input String', () => {
   expect(capitalizeWord('mephistoDevelop')).toBe('MephistoDevelop');
@@ -13,19 +13,19 @@ test('Reverse a sting', () => {
 });
 
 test('Sum of two numbers', () => {
-  expect(calculate['add'](1, 2)).toBe(3);
+  expect(calculate.add(1, 2)).toBe(3);
 });
 
 test('Substract from two numbers', () => {
-  expect(calculate['substract'](10, 5)).toBe(5);
+  expect(calculate.substract(10, 5)).toBe(5);
 });
 
 test('Multiply of two numbers', () => {
-  expect(calculate['multiply'](5, 2)).toBe(10);
+  expect(calculate.multiply(5, 2)).toBe(10);
 });
 
 test('Division from two numbers', () => {
-  expect(calculate['division'](10, 5)).toBe(2)
+  expect(calculate.division(10, 5)).toBe(2);
 });
 
 test('Caesar Cipher with 1 key value', () => {
@@ -45,7 +45,12 @@ test('Caesar Cipher with 1 key value with signs', () => {
 });
 
 test('simple array test 1', () => {
-  expect(arrayAnalysis([10, 20, 30, 40, 50])).toEqual({ average: 30, min: 10, max: 50, length: 5 });
+  expect(arrayAnalysis([10, 20, 30, 40, 50])).toEqual({
+    average: 30,
+    min: 10,
+    max: 50,
+    length: 5,
+  });
 });
 
 test('simple array test 2', () => {
@@ -53,16 +58,15 @@ test('simple array test 2', () => {
     average: 4,
     min: 1,
     max: 8,
-    length: 6
+    length: 6,
   });
 });
-
 
 test('simple array test 3', () => {
   expect(arrayAnalysis([5, 25, 50, 0, 0, 100, 5, 5, 5, 5])).toEqual({
     average: 20,
     min: 0,
     max: 100,
-    length: 10
+    length: 10,
   });
 });
