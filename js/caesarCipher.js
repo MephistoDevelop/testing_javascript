dc1d4f64138d20acf83bc00e6962337c5c83807b
MephistoDevelop <inesherald@gmail.com>
2020-01-23 07:52:24 -0600

    Fix linter file

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,9 +1,8 @@
-
 function decrypt(charArray, key) {
   const array = [];
   charArray.forEach((c) => {
-    if (c === 32) array.push(32)
-    if (c >= 33 && c <= 47) array.push(c)
+    if (c === 32) array.push(32);
+    if (c >= 33 && c <= 47) array.push(c);
     if (c >= 65 && c <= 90) {
       let enc = c + key;
       if (enc > 90) enc -= 26;
@@ -24,9 +23,8 @@ function decrypt(charArray, key) {
 }
 
 function caesarCypher(text = 'Hello world', key = 0) {
-  const charArray = text.split('').map(c => c.charCodeAt());
+  const charArray = text.split('').map((c) => c.charCodeAt());
   return decrypt(charArray, key);
 }
-
 
 module.exports = caesarCypher;
--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,6 @@
-
 const add = (n1, n2) => {
   return n1 + n2;
-}
+};
 const substract = (n1, n2) => {
   return n1 - n2;
 };
