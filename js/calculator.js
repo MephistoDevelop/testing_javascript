055e50aee19d86e7551600d177764bb6f9571ef2
MephistoDevelop <inesherald@gmail.com>
2020-01-23 07:57:50 -0600

    Fix linter file

--- js/calculator.js
+++ js/calculator.js
@@ -8,7 +8,7 @@ const multiply = (n1, n2) => {
   return n1 * n2;
 };
 const division = (n1, n2) => {
-  return n1 / n2;
+  return { n1 / n2};
 };
 
 module.exports = { add, substract, multiply, division };
